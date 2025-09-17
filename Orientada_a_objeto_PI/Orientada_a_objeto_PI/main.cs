f7a90a3be2fb7d3d6352a2e1d4dc4d6ff6f2bd1c
Pedro-Picolo <pedrohenriquearaujodelpicolo@gmail.com>
2025-09-16 22:40:23 -0300

    Add files via upload

---
+++ Orientada_a_objeto_PI/Orientada_a_objeto_PI/main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientada_a_objeto_PI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Usuarios user1 = new Usuarios("Pedro", "pedro@email.com", "1234", 1);
+        user1.ExibirInfo();
+
+
+    }
+}
