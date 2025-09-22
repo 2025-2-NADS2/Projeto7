8228d3a24c3c6bf35ab9bb3590ce42300e81372b
Matheus <162715868+Math19102012@users.noreply.github.com>
2025-09-22 16:44:55 -0300

    Add files via upload

---
+++ Documentos/Entrega 1/Programação Orientada a Objetos/Orientada_a_objeto_PI/Campanha.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientada_a_objeto_PI
+{
+    public class Campanha
+    {
+    public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public string LocalDoacao { get; set; }
+
+        public Campanha(string nome, DateTime dataInicio, DateTime dataFim, string localDoacao)
+        {
+            Nome = nome;
+            DataInicio = dataInicio;
+            DataFim = dataFim;
+            LocalDoacao = localDoacao;
+        }
+        public void ExibirInfo()
+        {
+            Console.WriteLine("Campanha:" +Nome);
+            Console.WriteLine("Início: "+DataInicio);
+            Console.WriteLine("Fim: "+DataFim);
+            Console.WriteLine("Local de Doação: "+LocalDoacao);
+        }
+
+        public bool EstaAtiva()
+        {
+            DateTime hoje = DateTime.Now;
+            return hoje >= DataInicio && hoje <= DataFim;
+        }
+    }
+}
---
+++ Documentos/Entrega 1/Programação Orientada a Objetos/Orientada_a_objeto_PI/Usuarios.cs
@@ -0,0 +1,32 @@
+﻿public class Usuarios
+{
+    // Atributos (Propriedades automáticas)
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public int ID { get; set; }
+
+    // Construtor
+    public Usuarios(string nome, string email, string senha, int id)
+    {
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+        ID = id;
+    }
+
+    public Usuarios()
+    {
+        Nome = "pedro";
+        Email = "pedro@teste.com";
+        Senha = "1234";
+        ID = 1;
+        // Construtor padrão
+    }
+
+    // Métodos
+    public void ExibirInfo()
+    {
+        Console.WriteLine($"Nome: {Nome}, Email: {Email}, ID: {ID}");
+    }
+}
